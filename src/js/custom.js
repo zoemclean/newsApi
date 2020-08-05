b7be8ae75a4ebff2ff05f4d8b0c2c2aa8cbe511e
sophiecrilly <crilly.sophie16@gmail.com>
2020-08-06 11:21:24 +1200

    made search bar work in console

--- src/js/custom.js
+++ src/js/custom.js
@@ -17,7 +17,9 @@ var newsapi = require('newsapi');
 			aboutPage: 'About Us',
 			homePage: 'Home',
     		news: false,
-			display: true
+			newsData: false,
+			display: true,
+			searchQuery: false
   		},
 		methods: {
 			goAboutPage: function () {
@@ -27,9 +29,38 @@ var newsapi = require('newsapi');
 			goHomePage: function () {
 				console.log('working switch')
 				this.display = true;
-			}
+			},
+			search: function () {
+				var searchQuery = document.getElementById("searchBar").value
+				app.searchQuery = searchQuery
+				console.log(searchQuery)
+				if (searchQuery != null) {
+					console.log('search working')
+					axios({
+						method: 'get',
+						url: 'http://newsapi.org/v2/everything?domains=wsj.com&q='+ app.searchQuery + '&apiKey=557ae4d31b5a4ef59cfe08919181c390'
+					})
+						.then(function (response) {
+							app.newsData = response;
+							app.news = response.data.articles;
+						}); // Request ENDS
+				} // If ENDS
+			} // Search Functuin ENDS
 		}
+
 	});
+
+	// function goSearch (word) {
+	// 	axios({
+	// 		method: 'get',
+	// 		url: 'http://newsapi.org/v2/everything?domains=wsj.com&q=' + word + '&apiKey=557ae4d31b5a4ef59cfe08919181c390'
+	// 	})
+	// 	.then(function (response){
+	// 		console.log(response.data);
+	// 		app.news = response.data;
+	// 	});
+	// }
+
 
 
 	axios({
@@ -39,7 +70,7 @@ var newsapi = require('newsapi');
 	.then(function (response){
 		app.news = response;
 		console.log(response);
-	})
+	});
 
 	console.log(app);
 
